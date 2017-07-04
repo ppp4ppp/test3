8b30272ad5d2d654dc0d3112d643b2f50f0d56ee
ppp4ppp <petr.ignatiev@gmail.com>
2017-07-03 22:53:11 -0500

    RedisT fam

--- src/Database/Redis/RedisT.hs
+++ src/Database/Redis/RedisT.hs
@@ -1,7 +1,20 @@
 module Database.Redis.RedisT where
 
 import Database.Redis
+import Control.Monad 
 
 data RedisT r m a = RedisT { runRedisT ::  r -> m a } 
 
+instance (Functor m) => Functor (RedisT r m) where
+  fmap f v = RedisT $ \ r -> fmap f (runRedisT v r)
+
+instance (Monad m) => Applicative (RedisT r m) where
+  pure a = RedisT $ \ r -> do (pure a)
+  (<*>) = ap
+
+instance (Monad m) => Monad (RedisT r m) where
+  return a = RedisT $ \ r -> do return a
+  v >>= f = RedisT $ \ r -> do 
+                      b <- (runRedisT v r)
+                      runRedisT (f b) r
 
