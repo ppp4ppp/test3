d4ab73a9e87cfded4ad2bdfaf3ef6046be5993a9
ppp4ppp <petr.ignatiev@gmail.com>
2017-07-04 11:49:37 -0500

    done ReaderT no resouce

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad.IO.Class
+
+import   Data.ByteString
+import Data.Conduit
+import Data.Conduit.List
+
+import Database.Redis
+
+import Database.Redis.RedisT
 import Testrs
 
+
+sinkPrint :: (Show i, MonadIO m) => Sink i m ()
+sinkPrint = awaitForever $ \ x -> liftIO $ print x
+
+sinkSet :: (MonadRedis m) => (i -> (ByteString, ByteString)) -> Sink i m ()
+sinkSet kv = awaitForever $ \ x -> liftRedis $ do (uncurry set) (kv x); return ()
+
+condSet :: (MonadRedis m) => (i -> (ByteString, ByteString)) -> Conduit i m i 
+condSet kv = awaitForever $ \ x -> do
+                            liftRedis (do (uncurry set) (kv x); return ())
+                            yield x
+
+
+--kv :: ()
+
 main :: IO ()
-main = someFunc
+main = do
+  print "done"
+  conn <- checkedConnect defaultConnectInfo
+  runRedisT (sourceList [1..10] .| condSet ( \ _ -> ( "test", "testval")) $$ sinkPrint) conn
+
--- src/Database/Redis/RedisT.hs
+++ src/Database/Redis/RedisT.hs
@@ -1,11 +1,19 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Database.Redis.RedisT where
 
-import Database.Redis
+
 import Control.Monad 
+import Control.Monad.Trans
+
+import Data.Conduit
+
+import Database.Redis
+
 
 data RedisT r m a = RedisT { runRedisT ::  r -> m a } 
 
-instance (Functor m) => Functor (RedisT r m) where
+instance (Functor m) => Functor (RedisT r m) where  
   fmap f v = RedisT $ \ r -> fmap f (runRedisT v r)
 
 instance (Monad m) => Applicative (RedisT r m) where
@@ -18,3 +26,18 @@ instance (Monad m) => Monad (RedisT r m) where
                       b <- (runRedisT v r)
                       runRedisT (f b) r
 
+instance MonadTrans (RedisT Connection) where
+  lift = liftRedisT
+
+instance  MonadRedis (RedisT Connection IO) where
+  liftRedis r = RedisT $ \ conn -> runRedis conn r
+
+instance (MonadRedis m) => MonadRedis (ConduitM i o m) where
+  liftRedis = lift . liftRedis
+
+instance (MonadIO m) => MonadIO (RedisT Connection m) where
+  liftIO = lift . liftIO
+
+
+liftRedisT :: m a -> RedisT r m a
+liftRedisT m = RedisT (const m)
