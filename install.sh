a653333977b5b5c7be9576f0de428bb33a4cf7a4
ppp4ppp <petr.ignatiev@gmail.com>
2025-10-03 12:56:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 echo "installing . . . . . !!!"
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/master/vynet.html > /home/vynet/steam-rtsp/client/vynet.html
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/master/bundle.js > /home/vynet/steam-rtsp/client/bundle.js
-wget -qO- https://raw.githubusercontent.com/ppp4ppp/refs/heads/master/vynet.html > /home/vynet/steam-rtsp/client/vynet.html
-wget -qO- https://raw.githubusercontent.com/ppp4ppp/refs/heads/master/bundle.js > /home/vynet/steam-rtsp/client/bundle.js
-
-
 echo "done"
 
