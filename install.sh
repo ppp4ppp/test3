0ebd8619c18faddd08151db81f9449834554c6f0
petr <petr@petr>
2025-10-03 12:34:42 -0500

    in work

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/refs/heads/master/vynet.html > /home/petr/work/misc/test3
-wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/refs/heads/master/bundle.js > /home/petr/work/misc/test3
-
+echo "installing..."
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/vynet.html > /home/petr/work/misc/test3/vynet.html
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bundle.js > /home/petr/work/misc/test3/bundle.js
+echo "done"
 
