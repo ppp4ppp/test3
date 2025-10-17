1436b33b42f57c0357af3519bd5d009a8fe088fb
petr <petr@petr>
2025-10-17 10:46:05 -0500

    in work

--- installv3.sh
+++ installv3.sh
@@ -3,6 +3,7 @@ wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/vynetv3.html > 
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/layoutv3.css > /home/vynet/steam-rtsp/client/layoutv3.css
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/videocontrolv3.css > /home/vynet/steam-rtsp/client/videocontrolv3.css
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/navv3.css > /home/vynet/steam-rtsp/client/navv3.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/configv3.json > /home/vynet/steam-rtsp/client/config.json
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bundlev3.js > /home/vynet/steam-rtsp/client/bundle.js
 echo "done"
 
