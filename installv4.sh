069fd306157d44450fb5f4238ee1e3b00597d17a
petr <petr@petr>
2025-10-21 15:54:54 -0500

    in work

---
+++ installv4.sh
@@ -0,0 +1,10 @@
+echo "installing v4 ..."
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/vynetv4.html > /home/vynet/steam-rtsp/client/vynet.html
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/layoutv4.css > /home/vynet/steam-rtsp/client/layoutv4.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/videocontrolv4.css > /home/vynet/steam-rtsp/client/videocontrolv4.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/navv4.css > /home/vynet/steam-rtsp/client/navv4.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/configv4.json > /home/vynet/steam-rtsp/client/config.json
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bundlev4.js > /home/vynet/steam-rtsp/client/bundle.js
+echo "done"
+
+
