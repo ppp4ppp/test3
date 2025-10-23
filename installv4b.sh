fd646e54c993498345df8b64a678daf95d3d42ab
petr <petr@petr>
2025-10-23 16:00:10 -0500

    in work

---
+++ installv4b.sh
@@ -0,0 +1,9 @@
+echo "installing v4 ....."
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/vynetv4a.html > /home/vynet/steam-rtsp/client/vynet.html
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/layoutv4.css > /home/vynet/steam-rtsp/client/layoutv4.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/videocontrolv4.css > /home/vynet/steam-rtsp/client/videocontrolv4.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/navv4.css > /home/vynet/steam-rtsp/client/navv4.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/configv4.json > /home/vynet/steam-rtsp/client/config.json
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bundlev4a.js > /home/vynet/steam-rtsp/client/bundle.js
+echo "done"
+
---
+++ installv5.sh
@@ -0,0 +1,13 @@
+echo "installing v4 ....."
+mkdir -p /home/vynet/steam-rtsp/client/imgs && \
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/fsn.png > /home/vynet/steam-rtsp/client/imgs/fsn.png
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/vynet3.png > /home/vynet/steam-rtsp/client/imgs/vynet3.png
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bulma.min.css > /home/vynet/steam-rtsp/client/bulma.min.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/vynetv5.html > /home/vynet/steam-rtsp/client/vynet.html
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/layoutv5.css > /home/vynet/steam-rtsp/client/layout.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/videocontrolv5.css > /home/vynet/steam-rtsp/client/videocontrol.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/navv5.css > /home/vynet/steam-rtsp/client/nav.css
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/configv5.json > /home/vynet/steam-rtsp/client/config.json
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bundlev5.js > /home/vynet/steam-rtsp/client/bundle.js
+echo "done"
+
