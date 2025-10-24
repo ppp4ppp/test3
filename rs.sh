335455c4b3276f70b62c80f8016d3ef59afd69a1
petr <petr@petr>
2025-10-24 17:41:44 -0500

    in work

--- rs.sh
+++ rs.sh
@@ -2,9 +2,9 @@ cp ../steam-rtsp/client/vynet.html vynet6.html
 cp ../steam-rtsp/client/imgs/fsn.png  fsn.png
 cp ../steam-rtsp/client/imgs/vynet3.png  vynet3.png
 cp ../steam-rtsp/client/bulma.min.css  bulma.min.css
-cp ../steam-rtsp/client/vynet.html  vynetv6a.html
-cp ../steam-rtsp/client/layout.css  layoutv6.css
-cp ../steam-rtsp/client/videocontrol.css videocontrolv6.css
-cp ../steam-rtsp/client/nav.css  navv6.css
-cp ../steam-rtsp/client/config.json  configv6.json
-cp ../steam-rtsp/client/bundle.js  bundlev6.js
+cp ../steam-rtsp/client/vynet.html  vynetv7a.html
+cp ../steam-rtsp/client/layout.css  layoutv7.css
+cp ../steam-rtsp/client/videocontrol.css videocontrolv7.css
+cp ../steam-rtsp/client/nav.css  navv7.css
+cp ../steam-rtsp/client/config.json  configv7.json
+cp ../steam-rtsp/client/bundle.js  bundlev7.js
