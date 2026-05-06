3263a28c01f41fa3e97f3a486f1b616a0532b32b
petr <petr@petr>
2026-05-06 15:52:10 -0500

    in work

--- installv15a.sh
+++ installv15a.sh
@@ -1,4 +1,4 @@
-echo "installing v15 ....."
+echo "installing v15a ....."
 mkdir -p /home/vynet/steam-rtsp/client/imgs && \
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/fsn.png > /home/vynet/steam-rtsp/client/imgs/fsn.png
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/vynet3.png > /home/vynet/steam-rtsp/client/imgs/vynet3.png
