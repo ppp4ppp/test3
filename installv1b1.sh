a70abbbc638949e7f4470bb0e250749f9da7d652
petr <petr@petr>
2026-09-25 11:23:10 -0500

    in work

---
+++ installv1b1.sh
@@ -0,0 +1,6 @@
+echo "installing v1 build1 ....."
+mkdir -p /home/vynet/steam-rtsp/client/imgs && \
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/targetv1.html > /home/vynet/steam-rtsp/client/target.html
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bundlev2b1.js > /home/vynet/steam-rtsp/client/bundlev2.js
+echo "done"
+
