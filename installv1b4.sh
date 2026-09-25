003174401470a7bc2107fc0b25f88cb82e707f90
petr <petr@petr>
2026-09-25 12:21:53 -0500

    in work

--- installv1b4.sh
+++ installv1b4.sh
@@ -1,6 +1,6 @@
 echo "installing v1 build3 ....."
 mkdir -p /home/vynet/steam-rtsp/client/imgs && \
-wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/targetv1.html > /home/vynet/steam-rtsp/client/target.html
+wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/targetv4.html > /home/vynet/steam-rtsp/client/target.html
 wget -qO- https://raw.githubusercontent.com/ppp4ppp/test3/master/bundlev2b4.js > /home/vynet/steam-rtsp/client/bundlev2.js
 touch /home/vynet/steam-rtsp/client/init
 echo "done"
